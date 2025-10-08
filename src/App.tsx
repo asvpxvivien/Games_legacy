2b6eb436029eb16cf94db55dcf29cbac6bcc5621
Vivien Amagbegnon <amagbegnonvivien7@gmail.com>
2025-10-08 22:57:01 +0100

    Bug and Errors Fixed

--- src/App.tsx
+++ src/App.tsx
@@ -13,26 +13,28 @@ import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <FavoritesProvider>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/timeline" element={<Timeline />} />
-            <Route path="/discovery" element={<Discovery />} />
-            <Route path="/favorites" element={<Favorites />} />
-            <Route path="/analysis" element={<Analysis />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
-    </FavoritesProvider>
-  </QueryClientProvider>
-);
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <FavoritesProvider>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="/timeline" element={<Timeline />} />
+              <Route path="/discovery" element={<Discovery />} />
+              <Route path="/favorites" element={<Favorites />} />
+              <Route path="/analysis" element={<Analysis />} />
+              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </TooltipProvider>
+      </FavoritesProvider>
+    </QueryClientProvider>
+  );
+}
 
 export default App;
